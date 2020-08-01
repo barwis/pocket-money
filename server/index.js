a572f1d39c8cc3a3cc542e43d78737fe60f50542
barwis <bartosz.wisniewski@telegraph.co.uk>
2020-08-01 12:16:32 +0100

    added mongodb settings

--- server/index.js
+++ server/index.js
@@ -1,7 +1,34 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo')(session);
+const path = require('path');
+const createError = require('http-errors');
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => res.send('OK!'))
+module.exports = (config) => {
+    const app = express()
+    const port = 5000;
 
-app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(cookieParser());
+
+    app.use(session({
+        secret: 'very secret 12345',
+        resave: true,
+        saveUninitialized: false,
+        store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    }));
+
+    if (app.get('env') === 'development') {
+        app.locals.pretty = true;
+    }
+
+    app.get('/health', (req, res) => res.send(200, { status: 'OK' }));
+
+    app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
+
+    return app;
+}
---
+++ server/index.test.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var request = require('supertest');
+
+var app = express();
+var router = express.Router();
+
