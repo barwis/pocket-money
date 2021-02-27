2fded07362de4d17d671822587e8d375bd89f19d
barwis <bartosz.wisniewski@telegraph.co.uk>
2021-02-27 13:35:48 +0000

    SH-3 backend image endpoint

--- client/src/components/weather/index.js
+++ client/src/components/weather/index.js
@@ -8,41 +8,49 @@ import Widget from '../../containers/widget';
 import code from './countriesList';
 
 const Condition = styled.div`
-display: flex;
-flex-direction: row;
+display: grid;
+grid-gap: 30px;
 align-items: center;
-justify-content: center;
+grid-template-columns: 1fr 1fr;
+padding: 10px 33px 20px 60px;
 `;
 
 const Temp = styled.div`
 display: flex;
 flex-direction: column;
-margin-left: 20px;`;
-
+`;
 
 const TempReal = styled.div`
-    font-size: 550%;
-    font-weight: 300;
+	font-size: 55px;
+    line-height: 1;
+    font-weight: 100;
+	margin-top: -15px;
 `;
 
 
 const TempFeelsLike = styled.div`
-color: #777777`;
+	color: #afafaf;
+	font-size: 14px;
+	white-space: nowrap;
+`;
 
 const WeatherDetails = styled.ul`
-	display: flex;
-	justify-content: space-between;
-	margin: 20px 0;
-	padding: 0 40px;
-	width: 100%;
-    box-sizing: border-box;
+display: grid;
+grid-template-columns: repeat(4, 1fr);
+grid-template-rows: 1fr 1fr;
+margin: 0;
+grid-column-gap: 50px;
+padding: 0;
+width: 100%;
+
 `;
 
 
 const WeatherDetailListItem = styled.li`
 	display:  flex;
-	flex-direction: column;
-	text-align: center;
+    flex-direction: row;
+    text-align: center;
+    justify-content: center;
 
 	div {
 		display: block;
@@ -50,25 +58,54 @@ const WeatherDetailListItem = styled.li`
 
 	div:first-child {
 		font-size: 300%;
-		margin-bottom: 10px;
 	}
 	sup {
 		padding-left: 3px;
 	}
 `;
 
+const WeatherImage = styled.img`
+	display: block;
+    width: 112px;
+    height: 112px;
+	-webkit-filter: invert(100%);
+	opacity: ${props => (props.src ? '1' : '0')};
+
+	.cls-1 {
+		fill:none;
+		stroke:#f1c40f;
+		stroke-miterlimit:1;
+		stroke-width:3px;
+		shape-rendering:geometricPrecision;
+		stroke-linecap: round; 
+		stroke-linejoin: round;
+	}
+`;
 
 export const Weather = ({ data = [], loadData, ...props }) => {
-	useEffect(() => {
+	useEffect( () => {
 		loadData();
+
 	}, [loadData])
+
+	const getImageOrFallback = (path, fallback) => {
+		try {
+			require(path)
+			return path;
+		} catch (err) {
+			return fallback;
+		}
+	};
+	
 	return (
 		<Widget title={data.location && data.location.name} subtitle={data.location && code(data.location.country)} lastUpdated={new Date().toLocaleString()} onUpdateClick={loadData} {...props}>
 			<Condition>
 				<div>
-					{/* <img src={data.current && data.current.condition && data.current.condition.icon } alt={data.current && data.current.condition && data.current.condition.text }/> */}
-
-					<div>public folder: {process.env.PUBLIC_URL}</div>
+					 {/* <img src={data.current && data.current.condition && data.current.condition.icon } alt={data.current && data.current.condition && data.current.condition.text }/> */}
+					 {/* { data.current && <WeatherImage src="" />} */}
+					 <WeatherImage src={data.current.condition.image} alt={data.current.condition.text} />
+					 {/* { http://localhost:5000/img/weather/64x64/day/fallback/185.svg.png } */}
+					{/* <WeatherImage src="http://localhost:3000/weather/64x64/day/svg/113.svg" alt={data.current && data.current.condition.icon}/> */}
 				</div>
 				<Temp>
 					<TempReal>{(data.current && data.current.temp_c) || '-'}&deg;</TempReal>
@@ -78,22 +115,23 @@ export const Weather = ({ data = [], loadData, ...props }) => {
 			</Condition>
 			<WeatherDetails>
 					<WeatherDetailListItem>
-						<div className="icon-wind">
-						</div>
-						<div>{data.current && Math.round(data.current.wind_kph)}<sup>km/h</sup></div>
+						<div className="icon-wind"> </div>
 					</WeatherDetailListItem>
 					<WeatherDetailListItem>
 						<div className="icon-raindrop"></div>
-						<div>{data.current && data.current.humidity}<sup>%</sup></div>
 					</WeatherDetailListItem>
 					<WeatherDetailListItem>
 						<div className="icon-raindrops"></div>
-						<div>{data.current && data.current.precip_mm}<sup>mm</sup></div>
 					</WeatherDetailListItem>
 					<WeatherDetailListItem>
 						<div className="icon-cloud-download"></div>
-						<div>{data.current && data.current.pressure_mb}<sup>hPa</sup></div>
 					</WeatherDetailListItem>
+					<WeatherDetailListItem>{data.current && Math.round(data.current.wind_kph)}<sup>km/h</sup></WeatherDetailListItem>
+					<WeatherDetailListItem>{data.current && data.current.humidity}<sup>%</sup></WeatherDetailListItem>
+					<WeatherDetailListItem>{data.current && data.current.precip_mm}<sup>mm</sup></WeatherDetailListItem>
+					<WeatherDetailListItem>{data.current && data.current.pressure_mb}<sup>hPa</sup></WeatherDetailListItem>
+
+
 				</WeatherDetails>
 		</Widget>
 	)
