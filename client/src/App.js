2fbeeedcd55bfe7cab16ef3a9a3cbe08e0b9243d
barwis <lordzix@gmail.com>
2021-05-23 13:45:34 +0100

    temporary remove forecast

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 import DateTime from './components/dateTime';
 import Calendar from './components/calendar';
-import Forecast from './components/forecast';
+// import Forecast from './components/forecast';
 import Weather from './components/weather';
 import Recycle from './components/recycle';
 // import DateTimeWeather from './components/dateTimeWeather';
@@ -13,7 +13,6 @@ const App = () => (
 	<div className="app">
 		<DateTime/>
 		<Weather />
-		<Forecast />
 		<Recycle />
 		<Calendar/>
 	</div>
